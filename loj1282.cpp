29d01c4e428b919f6a5e12d44d4b8a6b022304a9
Asifur Rahman (Ashiq) <57610730+asifur-rahman1@users.noreply.github.com>
2021-06-01 23:28:07 +0600

    a

---
+++ loj1282.cpp
@@ -0,0 +1,21 @@
+//Date: 2021-06-01 23:27:59
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long 
+#define int long long
+#define pb push_back
+ll mod = 1e9 + 7;
+#define mp make_pair
+
+void solve(){
+    
+}
+
+int32_t main(){
+    int tt = 1;
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin >> tt;
+    while(tt--){
+        solve();
+    }
+}
